d50b5cd81ae1916e05d6362f527c02db098f0173
Nokvix <igor.gass2003@mail.ru>
2024-11-06 14:42:03 +0500

    Обновил файл version.php

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_open_course_materials'; // Полное имя плагина.
+$plugin->version  = 2021051702;                      // Текущая версия плагина.
+$plugin->release = '4.8.1-2 (2022112700 mdl-39)';    // Версия и сборка плагина.
+$plugin->requires = 2019111200;                      // Минимальная версия Moodle.
+$plugin->maturity = MATURITY_STABLE;                 // Уровень стабильности.
