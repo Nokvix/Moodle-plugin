4737aba1c4e50c79958b65e75a5c0786f20bc7dc
Nokvix <igor.gass2003@mail.ru>
2024-11-06 13:30:58 +0500

    Initial commit

---
+++ lang/en/local_open_course_materials.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Open Course Materials';
+$string['course_access_days'] = 'Days to course access';
+$string['course_access_days_desc'] = 'Number of days from user registration until course access opens.';
+$string['module_access_days'] = 'Days to module access';
+$string['module_access_days_desc'] = 'Number of days from course enrollment until module access opens.';
---
+++ lang/ru/local_open_course_materials.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Открыть материалы курса';
+$string['course_access_days'] = 'Дни до доступа к курсу';
+$string['course_access_days_desc'] = 'Количество дней с момента регистрации пользователя до открытия доступа к курсу';
+$string['module_access_days'] = 'Дни до доступа к модулю';
+$string['module_access_days_desc'] = 'Количество дней с момента зачисления на курс до открытия доступа к модулю';
