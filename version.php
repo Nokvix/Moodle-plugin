50abfab89abcba3ae96eb2828382ea214db10cf9
Egor Vastikov <egorvast@mail.ru>
2025-01-08 18:59:14 +0500

    обновил файл version.php (37 версия плагина)

--- version.php
+++ version.php
@@ -1,8 +1,15 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY.
+//
+// See the GNU General Public License for more details.
+
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'local_open_course_materials'; // Полное имя плагина.
-$plugin->version  = 2021051702;                      // Текущая версия плагина.
-$plugin->release = '4.8.1-2 (2022112700 mdl-39)';    // Версия и сборка плагина.
-$plugin->requires = 2019111200;                      // Минимальная версия Moodle.
-$plugin->maturity = MATURITY_STABLE;                 // Уровень стабильности.
+$plugin->version = 2024122037;  // Версия плагина.
+$plugin->release = '4.8.1-5 (2024042900 mdl-39)';    // Обновлённая версия и сборка плагина.
+$plugin->requires = 2019111200;     // Минимальная версия Moodle, например 3.9 (2019111800).
+$plugin->component = 'local_open_courses_and_materials_individually';
+$plugin->maturity = MATURITY_STABLE;
