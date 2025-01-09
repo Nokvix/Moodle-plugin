afadc2ac2e1048c22b1450dbae8ba708fd521ee8
Nokvix <igor.gass2003@mail.ru>
2025-01-09 20:32:12 +0500

    Зарегистрировал обработчик события для handle_course_viewed

--- db/events.php
+++ db/events.php
@@ -3,6 +3,12 @@ defined('MOODLE_INTERNAL') || die();
 
 $observers = [
     [
+        'eventname' => '\core\event\course_viewed',
+        'callback' => '\local_open_courses_and_materials_individually\observer::handle_course_viewed',
+        'internal' => false,
+        'priority' => 9999
+    ],
+    [
         'eventname' => '\core\event\user_enrolment_created',
         'callback' => '\local_open_courses_and_materials_individually\observer::handle_user_enrolment',
         'internal' => false
